50f889c871fa1b072e90fdcb1b2f8bf388d2b383
fernandohernandezpaz <fernandohernandezpaz@outlook.com>
2021-09-25 12:35:41 -0600

    adding laravel ui bootstrap

--- src/app/routes/web.php
+++ src/app/routes/web.php
@@ -19,3 +19,7 @@ Route::get('/', function () {
 });
 
 Route::resource('quotes', QuotesController::class);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
